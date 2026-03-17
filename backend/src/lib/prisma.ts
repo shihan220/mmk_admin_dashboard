422cd9b6d83ce2466bd2fb99cd3de74556674795
Mohammad Shihan <mohammadshihan@Mohammads-MacBook-Air.local>
2026-03-17 22:49:45 +0000

    Make main frontend-only

--- backend/src/lib/prisma.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { env } from '../config/env';
-
-export const prisma = new PrismaClient({
-  log: env.NODE_ENV === 'development' ? ['warn', 'error'] : ['error']
-});
--- backend/src/utils/async-handler.ts
+++
@@ -1,7 +0,0 @@
-import type { NextFunction, Request, RequestHandler, Response } from 'express';
-
-export const asyncHandler =
-  (handler: (req: Request, res: Response, next: NextFunction) => Promise<unknown>): RequestHandler =>
-  (req, res, next) => {
-    handler(req, res, next).catch(next);
-  };
